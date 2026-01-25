1b1e2f9edd864f15ae6185ebc3d5404462014589
Jbayne71 <jalenbayne@gmail.com>
2026-01-25 01:46:20 -0500

    edited features

--- components/studentpenny/features.tsx
+++ components/studentpenny/features.tsx
@@ -41,7 +41,7 @@ export function Features() {
           </p>
         </div>
 
-        <div className="mt-12 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="mt-12 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
           {features.map((feature) => (
             <Card
               key={feature.title}
